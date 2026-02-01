55f35dc41d26928c0fe5027a07cddedcbc5167b3
maxmaster <yousseflkashef000@gmail.com>
2026-02-01 10:39:16 +0200

    the greatest commit

---
+++ HAL/Push_button/btn_cfg.c
@@ -0,0 +1,10 @@
+#include "btn_cfg.h"
+
+
+const btn_config_t btn_cfg[BTN_NUM_MAX] = {
+    [BTN_NAME_0] = {
+        .pin = 8,
+        .port = GPIOA,
+        .pull = BTN_RESISTANCE_PULLUP_INTERNAL
+    },
+};
