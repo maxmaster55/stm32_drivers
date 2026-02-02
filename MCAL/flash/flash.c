bb38a3272b1069c4057f11f53859a148a3e1e05d
maxmaster <yousseflkashef000@gmail.com>
2026-02-02 16:22:58 +0200

    flash write works

--- MCAL/flash/flash.c
+++ MCAL/flash/flash.c
@@ -24,7 +24,7 @@ void flash_mass_erase()
 }
 
 
-void flash_sector_erase(flash_sector_t num)
+void flash_erase_sector(flash_sector_t num)
 {
     if (num >= FLASH_SECTOR_MAX) return; // tf you doin? 
     
@@ -34,11 +34,52 @@ void flash_sector_erase(flash_sector_t num)
     FLASH->CR.STRT = 1;
     while (FLASH->SR.BSY);
 }
-
-
-void flash_program()
+void flash_write(uint32_t address, uint32_t data, flash_w_len_t len)
 {
-    
+    if (len >= FLASH_WRITE_MAX) return;
+
+    // Alignment check (THIS PREVENTS PGAERR)
+    if ((len == FLASH_WRITE_HALFWORD && (address & 0x1)) ||
+        (len == FLASH_WRITE_WORD     && (address & 0x3)) ||
+        (len == FLASH_WRITE_DOUBLE   && (address & 0x7)))
+        return;
+
+    // Wait until flash is idle
+    while (FLASH->SR.BSY);
+
+    // Clear previous errors
+    FLASH->SR.reg =
+        FLASH_SR_PGSERR |
+        FLASH_SR_PGPERR |
+        FLASH_SR_PGAERR |
+        FLASH_SR_WRPERR |
+        FLASH_SR_EOP;
+
+    FLASH->CR.PSIZE = len;
+
+    // Enable programming
+    FLASH->CR.PG = 1;
+
+    switch (len)
+    {
+        case FLASH_WRITE_BYTE:
+            *(volatile uint8_t *)address = (uint8_t)data;
+            break;
+
+        case FLASH_WRITE_HALFWORD:
+            *(volatile uint16_t *)address = (uint16_t)data;
+            break;
+
+        case FLASH_WRITE_WORD:
+            *(volatile uint32_t *)address = data;
+            break;
+
+        default:
+            FLASH->CR.PG = 0;
+            return;
+    }
+
+    while (FLASH->SR.BSY);
+
+    FLASH->CR.PG = 0;
 }
-
-
--- MCAL/flash/flash.h
+++ MCAL/flash/flash.h
@@ -5,6 +5,11 @@
 
 #define FLASH ((flash_regs_t*)0x40023C00)
 
+#define FLASH_SR_EOP     (1 << 0)
+#define FLASH_SR_WRPERR  (1 << 4)
+#define FLASH_SR_PGAERR  (1 << 5)
+#define FLASH_SR_PGPERR  (1 << 6)
+#define FLASH_SR_PGSERR  (1 << 7)
 
 typedef enum{
     FLASH_SECTOR_0,
@@ -16,12 +21,21 @@ typedef enum{
     FLASH_SECTOR_MAX,
 } flash_sector_t ;
 
+typedef enum{
+    FLASH_WRITE_BYTE,
+    FLASH_WRITE_HALFWORD,
+    FLASH_WRITE_WORD,
+    FLASH_WRITE_DOUBLE,
+    FLASH_WRITE_MAX,
+} flash_w_len_t ;
+
 
 
 
 void flash_unlock();
 void flash_lock();
 void flash_mass_erase();
-void flash_sector_erase(flash_sector_t num);
-void flash_program();
+void flash_erase_sector(flash_sector_t num);
+void flash_write(uint32_t address, uint32_t data, flash_w_len_t len);
+
 
